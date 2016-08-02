68f0cd30425f28a8d359154dbd5e2aedc9cb5d5d
Pavel <19paul91@mail.ru>
2016-08-02 22:12:03 +0300

    add final code

--- gallary/gallary/ViewController.swift
+++ gallary/gallary/ViewController.swift
@@ -8,38 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 	
-	@IBOutlet var imageView: UIImageView!
+	@IBOutlet weak var imageView: UIImageView!
 	
+	//Создаем переменную и приравниваем ее к классу который управляет фотографиями и видео
 	let imagePicker = UIImagePickerController()
 	
-	
+	//на кнопку вешаем что фотобиблиотека изначально не активна, следом обращаемся к фотобиблиотеки и потображаем действие в view controller
 	@IBAction func loadImageButtonTapped(sender: UIButton) {
 		imagePicker.allowsEditing = false
 		imagePicker.sourceType = .PhotoLibrary
-		
 		presentViewController(imagePicker, animated: true, completion: nil)
 	}
- 
+	
+	// сдесь указываем что сами будет выбирать картинку из фото лаборатории
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
 		imagePicker.delegate = self
 	}
 	
 	// MARK: - UIImagePickerControllerDelegate Methods
-	
+	// в функции пишем что условие при котором IBoutlet растягивается по размеру области и приравниваем к imgPicker и отображаем действия во viewControllere
 	func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-		if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
-			imageView.contentMode = .ScaleAspectFit
-			imageView.image = pickedImage
+		if let imgPicker = info[UIImagePickerControllerOriginalImage] as? UIImage {
+		imageView.contentMode = .ScaleAspectFit
+		imageView.image = imgPicker
 		}
-		
 		dismissViewControllerAnimated(true, completion: nil)
 	}
 	
-	func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+	// Пишем функцию для отображения во viewControllere
+	// MARK: - imagePickerControllerDidCancel Methods
+	func imagePickerControllerDidCancel(imgPicker: UIImagePickerController){
 		dismissViewControllerAnimated(true, completion: nil)
 	}
+	
 }
